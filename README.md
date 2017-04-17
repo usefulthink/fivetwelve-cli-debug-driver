d3b3f993bb1bc45d764bd9500536489234ca853d
Martin Schuhfuss <m.schuhfuss@gmail.com>
2017-04-18 00:22:39 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+fivetwelve-cli-debug-driver
+===========================
+
+A driver to help debugging your applications. Will simply output the
+current channel-values of the universe into your terminal.
---
+++ index.js
@@ -0,0 +1,57 @@
+import {screen, widget, helpers} from 'blessed';
+import colormap from 'colormap';
+
+const sprintf = helpers.sprintf;
+
+export default class CliDebugDriver {
+  constructor() {
+    this.screen = screen({smartCSR: true, autoPadding: true});
+    this.table = widget.table({
+      width: 161, height: '100%',
+      tags: true,
+      border: {
+        type: 'line',
+        fg: '#f0f0f0'
+      }
+    });
+
+    this.screen.append(this.table);
+    this.colormap = colormap({
+      colormap: 'hot',
+      nshades: 256
+    });
+
+    // Render the screen.
+    this.update(new Buffer(512));
+  }
+
+  update(buffer) {
+    this.table.setData(this.formatData(buffer, 16));
+    this.screen.render();
+  }
+
+  formatData(buffer, columns) {
+    const data = Array.from(buffer);
+    const ret = [];
+    for (let startIndex = 0; startIndex < buffer.length; startIndex += columns) {
+      const slice = data.slice(startIndex, startIndex + columns);
+      ret.push(slice.map((num, i) => {
+        const fg = this.colormap[num];
+
+        return sprintf(
+            `{#888888-fg}{bold}%-3d{/}|{${fg}-fg}%03d{/}`,
+            startIndex + i + 1,
+            num
+        );
+      }));
+    }
+
+    this.lastData = data;
+    return ret;
+  }
+
+  send(buffer, universe) {
+    // FIXME: multi-universe support (tabs?)
+    this.update(buffer);
+  }
+}
